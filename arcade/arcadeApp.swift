229247e2bb1027f188cebd407db1b867f410341e
Amit Jotwani <hello@ajot.me>
2026-03-07 01:15:43 -0500

    Initial Commit

---
+++ arcade/arcadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  arcadeApp.swift
+//  arcade
+//
+//  Created by Amit Jotwani on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct arcadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ arcadeTests/arcadeTests.swift
@@ -0,0 +1,17 @@
+//
+//  arcadeTests.swift
+//  arcadeTests
+//
+//  Created by Amit Jotwani on 3/7/26.
+//
+
+import Testing
+@testable import arcade
+
+struct arcadeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
